9634abe2e8771979865bdc347068e59e5caea44e
地主 龍一 <Ryuichi@jishu-no-MacBook-Air.local>
2014-08-12 00:05:48 +0900

    issue #3 プロジェクト新規作成

---
+++ OpenGLSample/OpenGLSample/JNViewController.h
@@ -0,0 +1,13 @@
+//
+//  JNViewController.h
+//  OpenGLSample
+//
+//  Created by 地主 龍一 on 2014/08/12.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JNViewController : UIViewController
+
+@end
